23362af8cc4c62aea576b607caff2b9379304ef5
Darlan Henrique <darlan.silva@ice.ufjf.br>
2019-11-01 15:45:48 -0300

    transformamos as paginas dos crud em php e adicionamos o link das paginas nos botoes do navbar e cruds

--- crudProdutos.php
+++ crudProdutos.php
@@ -16,44 +16,18 @@
 </head>
 
 <body>
-    <nav class="navbar nav-home navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-            <img src="img/logodiamante.png" width="50" height="40" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Produtos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contato</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Quem Somos</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="buscar..." aria-label="Search">
-                <button class="btn  my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
-            </form>
-        </div>
-    </nav>
-
+   <header>
+       <?php
+            include 'navbar.html'; 
+        ?>
+   </header>
     <div class="container mt-3">
         <div class="d-flex justify-content-between">
             <form class="form-inline search-crud my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="buscar..." aria-label="Search">
                 <button class="btn btn-outline-dark" type="submit"><i class="fas fa-search"></i></button>
             </form>
-            <button type="button" class="btn btn-outline-success">Novo Produto</button>
+            <a href="formularioCriarProduto.php"><button type="button" class="btn btn-outline-success">Novo Produto</button></a>
         </div>
         <table class="table mt-3">
             <thead class="thead-dark">
@@ -95,7 +69,7 @@
                 </tr>
             </tbody>
         </table>
-        <button type="button" class="btn btn-outline-dark"><i class="fas fa-arrow-left"></i> Voltar</button>
+        <a href="homeAdm.html"><button type="button" class="btn btn-outline-dark"><i class="fas fa-arrow-left"></i> Voltar</button></a>
 
     </div>
     <!-- Optional JavaScript -->
@@ -111,45 +85,10 @@
         crossorigin="anonymous"></script>
 </body>
 
-<footer id="rodape" class="mt-5 content">
-    <div>
-        <div class="text-center row">
-            <div class="col-md-4 mapaSite">
-                <a href="index.html"><img id="logo" src="img/logo.png"></a>
-            </div>
-            <div class="col-md-4 mapaSite">
-                <nav>
-                    <p>
-                        <h6>Nossa empresa</h6>
-                    </p>
-                    <p><a class="menu" href="index.html">Home</a></p>
-                    <p><a class="menu" href="produtos.html">Veja os nossos produtos</a></p>
-                    <p><a class="menu" href="quemsomos.html">Quem Somos</a></p>
-                    <p><a class="menu" href="viewContato.html">Entre em contato</a></p>
-                </nav>
-            </div>
-            <div id="end" class="col-md-4">
-                <sect-ion>
-                    <p for="email">
-                        <h6>Nos Acompanhe</h6>
-                    </p>
-                    <p id="end">E seja o primeiro a receber todas as novidades de nossa loja pelo o seu email.</p>
-                    <p><input class="UserEmail form-control" name="UserEmail" id="UserEmail" type="email"
-                            placeholder="Seu email">
-                    </p>
-                    <p><input class="btn btn-outline-dark bto" type="submit" value="Inscreva-se!" /></p>
-                    </section>
-                    <p><a title="Facebook" href="http://facebook.com/"><i class="fab fa-facebook-square social"></i></a>
-                        <a title="Instagram" href="http://instagram.com/"><i
-                                class="fab fa-instagram social"></i></i></a>
-                        <a title="Twitter" href="http://twitter.com/"><i class="fab fa-twitter social"></i></i></a>
-                        <a title="Whatsapp" href="http://web.whatsapp.com/"><i
-                                class="fab fa-whatsapp-square social"></i></a></p>
-            </div>
-        </div>
-        <p id="copyright">© Copyright 2019 - Todos os direitos reservados. <a href="viewLogin.html"><i
-                    class="fas fa-lock login"></i></a></p>
-    </div>
-</footer>
+<footer>
+    <?php
+        include 'rodape.html'; 
+    ?>
+    </footer>
 
 </html>
---
+++ formularioEditarProduto.php
@@ -0,0 +1,83 @@
+<head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+        <title>Paula Brincos</title>
+        <link rel="icon" href="img/logodiamante.png">
+    
+        <link rel="stylesheet" href="css/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/101f0cf20a.js" crossorigin="anonymous"></script>
+    </head>
+    
+    <body>
+    <header>
+       <?php
+            include 'navbar.html'; 
+        ?>
+   </header>
+    
+        <div class="container mt-3">
+            <div class="card">
+                    <h5 class="card-header"><p class="font-weight-bold">Editar Produto</p></h5>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">
+                                <p class="font-weight-bold">Nome:</p>
+                            </label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                                placeholder="digite o nome do produto">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">
+                                <p class="font-weight-bold">Categoria:</p>
+                            </label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                                <label for="exampleFormControlFile1"><p class="font-weight-bold">Imagem:</p></label>
+                                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                              </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">
+                                <p class="font-weight-bold">Descrição:</p>
+                            </label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-outline-success">Salvar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    </body>
+    
+    <footer>
+       <?php
+            include 'rodape.html'; 
+        ?>
+    </footer>
+    
+    </html>
