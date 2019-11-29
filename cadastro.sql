a250c17756abf8792657a2bb0f6c42f7cd6ba6f2
Rafaela Horta <rafaela.horta@engenharia.ufjf.br>
2019-11-29 18:46:52 -0300

    banco de dados atualizado

--- cadastro.sql
+++ cadastro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29-Nov-2019 às 21:05
+-- Tempo de geração: 29-Nov-2019 às 22:45
 -- Versão do servidor: 10.4.8-MariaDB
 -- versão do PHP: 7.1.32
 
@@ -41,7 +41,8 @@ CREATE TABLE `categoria` (
 INSERT INTO `categoria` (`id`, `nome`, `descricao`) VALUES
 (2, 'anel', ''),
 (3, 'brinco', ''),
-(4, 'colar', '');
+(4, 'colar', ''),
+(5, 'pulseira', 'acessorio para os pulsos.');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,10 @@ CREATE TABLE `produto` (
 --
 
 INSERT INTO `produto` (`id`, `nome`, `preco`, `descricao`, `url_imagem`, `data_venda`, `categoria_id`) VALUES
-(13, 'anel fofo', 50, 'de diamante', 'anel.jpg', '2019-11-14 03:00:00', 2);
+(13, 'Anel Fofo', 50, 'Anel de ouro 24 quilates.', 'anel.jpg', '0000-00-00 00:00:00', 2),
+(14, 'Brinco Fofo', 62, 'Brinco de diamantes.', 'brinco.jpg', '0000-00-00 00:00:00', 3),
+(15, 'Colar Doido', 2346, 'Reliquia doida.', 'colar.jpg', '0000-00-00 00:00:00', 4),
+(16, 'Pulseira Fofa', 5, 'Pulseira eira.', 'pulseira.jpg', '0000-00-00 00:00:00', 5);
 
 -- --------------------------------------------------------
 
@@ -118,13 +122,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
