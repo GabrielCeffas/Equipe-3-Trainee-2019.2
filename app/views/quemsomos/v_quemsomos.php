543fe8f2857f26f56d3189097ff1727997e9f7ec
Rafaela Horta <rafaela.horta@engenharia.ufjf.br>
2019-11-30 00:14:22 -0300

    adicionei missão e visão no quem somos

--- app/views/quemsomos/v_quemsomos.php
+++ app/views/quemsomos/v_quemsomos.php
@@ -18,14 +18,14 @@
       <p class="hist">
         <h3>Missão: </h3>
       </p>
-      <p class="texto_hist">Uma missao generica para a nossa empresa. Uma missao generica para a nossa empresa. Uma missao generica para a nossa empresa. </p>
+      <p class="texto_hist">A missão da Paula Brincos, hoje e sempre, é oferecer a todas as mulheres do mundo um universo maravilhoso de jóias, cujas peças são produzidas à mão, usando materiais de alta qualidade a um preço acessível. A joalharia Paula Brincos é genuína, contemporânea, feminina e inspira as mulheres a expressar a sua individualidade. </p>
     </div>
     <div class="col-md-4">
       <p><i class="fas fa-eye histo"></i></p>
       <p class="hist">
         <h3>Visão: </h3>
       </p>
-      <p class="texto_hist">Uma Visao genérica para a nossa empresa. Uma Visao genérica para a nossa empresa. Uma Visao genérica para a nossa empresa. </p>
+      <p class="texto_hist">Ser líder, inovar e ser reconhecida como uma empresa referência no segmento de acessórios de luxo, oferecendo produtos de alto padrão de qualidade. </p>
     </div>
     <div class="col-md-4">
       <p><i class="fas fa-star histo"></i></p>
